7cb3724bccaabfb3e8cb765f3605128fcef32ced
ajp64 <antonyjparnell@gmail.com>
2022-01-27 20:59:44 +0000

    deploy app to heroku

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import "./index.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  const loadData = async () => {
+    const response = await fetch("/history/artist?name=Opeth");
+    // const apiData = await response.json();
+    setData(await response.json());
+    console.log(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Spotify</h1>
+    </div>
+  );
+}
+export default App;

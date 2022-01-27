83fa9edf8170f6ac70053dbb116be54c0eadad55
ajp64 <antonyjparnell@gmail.com>
2022-01-27 21:11:06 +0000

    Added headers to fetch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,12 @@ function App() {
   const [data, setData] = useState(null);
 
   const loadData = async () => {
-    const response = await fetch("/history/artist?name=Opeth");
+    const response = await fetch("/history/artist?name=Opeth", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
     // const apiData = await response.json();
     setData(await response.json());
     console.log(data);

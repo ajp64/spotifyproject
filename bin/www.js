d81f17d371d854b8fbd6c923ffccd19158178325
ajp64 <antonyjparnell@gmail.com>
2022-01-31 14:22:51 +0000

    changed to https

--- bin/www.js
+++ bin/www.js
@@ -5,7 +5,7 @@
  */
 
 import debugLib from "debug";
-import http from "http";
+import https from "https";
 
 import app from "../app.js";
 
@@ -22,7 +22,7 @@ app.set("port", port);
  * Create HTTP server.
  */
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.

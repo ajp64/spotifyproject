dac0b68c9f3247830d63e2518397ba188cce2d13
ajp64 <antonyjparnell@gmail.com>
2022-01-31 18:52:39 +0000

    added db variable

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import pg from "pg";
 import { creds } from "../config.js";
 import "dotenv/config";
-const connectionString = creds.string;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new pg.Pool({
   // user: creds.user,

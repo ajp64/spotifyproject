6f9a19624e214978c4a22e908865483c87498ba1
ajp64 <antonyjparnell@gmail.com>
2022-02-16 22:41:46 +0000

    Number of plays added for each track

--- client/src/components/Songs/index.js
+++ client/src/components/Songs/index.js
@@ -7,13 +7,31 @@ function onlyUnique(value, index, self) {
 
 export function Songs({ artist }) {
   const [songs, setSongs] = useState([]);
+  const [plays, setPlays] = useState([]);
   const url = `https://my-spotify-2021.herokuapp.com/artist?name=${artist}`;
   const { data } = useFetch(url);
 
   useEffect(() => {
     if (data) {
       let filteredSongs = data.payload.filter(onlyUnique);
+      let countedNames = data.payload.reduce(function (allNames, name) {
+        if (name.track in allNames) {
+          allNames[name.track]++;
+        } else {
+          allNames[name.track] = 1;
+        }
+        return allNames;
+      }, {});
+      console.log(countedNames);
+      let playsArray = [];
+      for (let i in countedNames) {
+        playsArray.push([i, countedNames[i]]);
+      }
+      playsArray.sort(function (a, b) {
+        return b[1] - a[1];
+      });
       setSongs(filteredSongs);
+      setPlays([...playsArray]);
     }
   }, [data]);
 
@@ -21,9 +39,15 @@ export function Songs({ artist }) {
     return (
       <>
         <h1>{artist}</h1>
-        <h2>{songs.length ? `Number of plays: ${data.payload.length}` : ""}</h2>
-        {songs.map((item, index) => {
-          return <h3 key={index}>{item.track}</h3>;
+        <h2>{songs.length ? `Total plays: ${data.payload.length}` : ""}</h2>
+        {plays.map((item, i) => {
+          return (
+            <h3 key={i}>
+              {item[1] === 1
+                ? `${item[0]} : ${item[1]} play`
+                : `${item[0]} : ${item[1]} plays`}
+            </h3>
+          );
         })}
       </>
     );

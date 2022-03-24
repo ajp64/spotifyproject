752eca1ce96362f78cc2a1013b95d23f88ecb37f
ajp64 <antonyjparnell@gmail.com>
2022-03-24 22:30:18 +0000

    fixed issue with fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
 import css from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Songs } from "./components/Songs";
 import { ArtistInput } from "./components/ArtistInput";
 import FadeIn from "react-fade-in";
 import { Container } from "@chakra-ui/react";
+import base64 from "base-64";
+
+let url = "https://accounts.spotify.com/api/token";
+let username = process.env.REACT_APP_CLIENT_ID;
+let password = process.env.REACT_APP_CLIENT_SECRET;
+
+//headers.append('Content-Type', 'text/json');
+const header = {
+  Authorization: "Basic " + base64.encode(username + ":" + password),
+  "Content-Type": "application/x-www-form-urlencoded",
+};
 
 function App() {
   const [artist, setArtist] = useState(null);
+  const [accessToken, setToken] = useState(null);
+
+  async function authGet() {
+    const authdata = await fetch(url, {
+      method: "POST",
+      headers: header,
+      body: "grant_type=client_credentials",
+    });
+    const auth = await authdata.json();
+    setToken(auth.access_token);
+  }
+
+  useEffect(() => {
+    authGet();
+  }, []);
 
   function handleArtist(e) {
     if (e.key === "Enter") {
@@ -26,7 +52,7 @@ function App() {
             artist={artist}
           />
           <Container maxW="container.xl">
-            <Songs artist={artist} />
+            <Songs artist={artist} accessToken={accessToken} />
           </Container>
         </FadeIn>
       ) : (
